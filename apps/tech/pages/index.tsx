e13586d1ba65cfcaa931b1511f19d3d2d50aa368
clodal <joel@onextech.com>
2023-03-28 19:05:06 +0800

    fix: pass broken tests due to getStaticProps not being available in jest (todo)

--- apps/tech/pages/index.tsx
+++ apps/tech/pages/index.tsx
@@ -3,6 +3,7 @@ import LandingLayout from '@app/layouts/LandingLayout'
 import { ShowcasesPage, ShowcasesPageProps } from '@onex/pages'
 import configs from '@app/configs'
 import { ShowcaseList } from '@onex/modules'
+import { MOCK_TECH_SHOWCASES } from '@onex/mocks'
 
 export const getStaticProps = ShowcaseList.getStaticProps({ configs })
 
@@ -12,7 +13,7 @@ const NextHomePage: React.FC<NextHomePageProps> = (props) => {
   const { showcases } = props
   return (
     <LandingLayout seo={{ title: 'Home' }}>
-      <ShowcasesPage showcases={showcases} />
+      <ShowcasesPage showcases={showcases || MOCK_TECH_SHOWCASES} />
     </LandingLayout>
   )
 }
