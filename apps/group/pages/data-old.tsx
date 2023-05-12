e341d6174d58cb28b2b8e007589e79e875ca5aa1
clodal <joel@onextech.com>
2023-05-12 15:08:07 +0800

    build: fix build errors

--- apps/group/pages/data-old.tsx
+++ apps/group/pages/data-old.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { LandingLayout } from '@onex/layouts'
 import { DataPage, DataPageProps } from '@onex/pages'
-import { MOCK_DATA_PAGE, MOCK_TECH_SHOWCASES } from '@onex/mocks'
+import { MOCK_DATA_PAGE, MOCK_SHOWCASES } from '@onex/mocks'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { fetchSite, getDynamicPage, makeGetStaticProps } from '@onex/server'
 import { PageProvider } from '@onex/providers'
 
+const { MOCK_KEY } = process.env
+
 export const getStaticProps: GetStaticProps = (context) => {
   const site = fetchSite()
   const data = getDynamicPage({ context, page: MOCK_DATA_PAGE, site })
-  const showcases = MOCK_TECH_SHOWCASES
+  const showcases = MOCK_SHOWCASES[MOCK_KEY]
   return makeGetStaticProps({ props: { data, showcases } })(context)
 }
 
--- apps/group/pages/design.tsx
+++ apps/group/pages/design.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { LandingLayout } from '@onex/layouts'
 import { DesignPage, DesignPageProps } from '@onex/pages'
-import { MOCK_DESIGN_PAGE, MOCK_TECH_SHOWCASES } from '@onex/mocks'
+import { MOCK_DESIGN_PAGE, MOCK_SHOWCASES } from '@onex/mocks'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { fetchSite, getDynamicPage, makeGetStaticProps } from '@onex/server'
 import { PageProvider } from '@onex/providers'
 
+const { MOCK_KEY } = process.env
+
 export const getStaticProps: GetStaticProps = (context) => {
   const site = fetchSite()
   const design = getDynamicPage({ context, page: MOCK_DESIGN_PAGE, site })
-  const showcases = MOCK_TECH_SHOWCASES
+  const showcases = MOCK_SHOWCASES[MOCK_KEY]
   return makeGetStaticProps({ props: { design, showcases } })(context)
 }
 
