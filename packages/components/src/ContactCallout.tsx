57cb83a39ca2681ab4641dc1a623a88b02f7984c
shank1xt <shashank.acharya@onextech.com>
2023-05-23 12:29:31 +0800

    feat: add option to make callout subtle

--- packages/components/src/ContactCallout.tsx
+++ packages/components/src/ContactCallout.tsx
@@ -3,14 +3,17 @@ import { Block } from '@gravis-os/landing'
 import { renderContactCalloutButtonBlockItem } from '@onex/blocks'
 import { useLayout } from '@onex/providers'
 
-export interface ContactCalloutProps {}
+export interface ContactCalloutProps {
+  isSubtle?: boolean
+}
 
 const ContactCallout: React.FC<ContactCalloutProps> = (props) => {
   const { site } = useLayout()
   const { cta_title, cta_button_title } = site
+  const { isSubtle } = props
 
   const footerCalloutBlockProps = {
-    py: 8,
+    py: isSubtle ? 3 : 8,
     sx: {
       backgroundColor: 'background.paper',
       borderTop: 1,
@@ -21,32 +24,32 @@ const ContactCallout: React.FC<ContactCalloutProps> = (props) => {
     items: [
       {
         type: 'grid',
-        gridProps: { spacing: 3 },
+        gridProps: { spacing: isSubtle ? { xs: 1, md: 2 } : 3 },
         gridItems: [
           {
-            md: 12,
+            md: isSubtle ? 8 : 12,
             boxProps: {
               sx: {
                 height: '100%',
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'center',
+                justifyContent: isSubtle ? { xs: 'center', md: 'flex-start' } : 'center',
               },
             },
             items: [
               {
-                type: 'h4',
+                type: isSubtle ? 'h5' : 'h4',
                 title: cta_title,
                 titleProps: {
-                  sx: { textAlign: 'center' },
+                  sx: { textAlign: isSubtle ? { xs: 'center', md: 'left' } : 'center' },
                   component: 'h5'
                 },
               },
             ],
           },
           {
-            md: 12,
-            boxProps: { sx: { textAlign: 'center' } },
+            md: isSubtle ? 4 : 12,
+            boxProps: { sx: { textAlign: isSubtle ? { xs: 'center', md: 'right' } : 'center' } },
             items: [
               renderContactCalloutButtonBlockItem({ title: cta_button_title }),
             ],
