24e3beedb716223bd2bf93868aa4734c9afa3011
shank1xt <shashank.acharya@onextech.com>
2023-05-26 15:12:27 +0800

    fix: seo issues

--- apps/data/pages/industries/[slug].tsx
+++ apps/data/pages/industries/[slug].tsx
@@ -17,7 +17,13 @@ const NextIndustryPage: React.FC<NextIndustryPageProps> = (props) => {
   const { industry, relatedPosts, pageProviderProps } = props
   return (
     <PageProvider {...pageProviderProps}>
-      <LandingLayout seo={industry.seo} autoBreadcrumbs>
+      <LandingLayout
+        seo={{
+          title: industry.title,
+          description: `Discover how One X Design revolutionizes ${industry.title.toLowerCase()} with unique design solutions. Explore insights and stay ahead in the market.`,
+        }}
+        autoBreadcrumbs
+      >
         <IndustryPage industry={industry} relatedPosts={relatedPosts} />
       </LandingLayout>
     </PageProvider>
