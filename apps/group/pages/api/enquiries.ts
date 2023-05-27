f85a963f1f24568a470cbe71537d29170cb2a135
clodal <joel@onextech.com>
2023-05-27 16:51:01 +0800

    fix: add edgeConfig to all apps

--- apps/group/pages/api/enquiries.ts
+++ apps/group/pages/api/enquiries.ts
@@ -2,7 +2,7 @@ import { handlePostEnquiry } from '@onex/modules'
 
 export const config = {
   runtime: 'edge',
-  regions: ['sin1']
+  regions: ['sin1'],
 }
 
 export default handlePostEnquiry
--- apps/tech/pages/api/enquiries.ts
+++ apps/tech/pages/api/enquiries.ts
@@ -1,3 +1,8 @@
 import { handlePostEnquiry } from '@onex/modules'
 
+export const config = {
+  runtime: 'edge',
+  regions: ['sin1'],
+}
+
 export default handlePostEnquiry
