476b6a643733a2ba4c393866bc070b4f5a3dc171
shank1xt <shashank.acharya@onextech.com>
2023-06-01 12:23:28 +0800

    fix: lint issues

--- apps/group/pages/resources/[slug]/success.tsx
+++ apps/group/pages/resources/[slug]/success.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { LandingLayout } from '@onex/layouts'
 import { ResourcePageProps, ResourceSuccessPage } from '@onex/pages'
 
-import { ResourceDetail, ResourceList } from '@onex/server'
+import { ResourceDetail } from '@onex/server'
 import { PageProvider } from '@onex/providers'
 import { InferGetStaticPropsType } from 'next'
 
@@ -13,12 +13,17 @@ export interface NextResourceSuccessPageProps
   extends InferGetStaticPropsType<typeof getStaticProps>,
     ResourcePageProps {}
 
-const NextResourceSuccessPage: React.FC<NextResourceSuccessPageProps> = (props) => {
+const NextResourceSuccessPage: React.FC<NextResourceSuccessPageProps> = (
+  props
+) => {
   const { resource, relatedResources, pageProviderProps } = props
   return (
     <PageProvider {...pageProviderProps}>
       <LandingLayout seo={{ title: 'Thank You' }}>
-        <ResourceSuccessPage resource={resource} relatedResources={relatedResources} />
+        <ResourceSuccessPage
+          resource={resource}
+          relatedResources={relatedResources}
+        />
       </LandingLayout>
     </PageProvider>
   )
