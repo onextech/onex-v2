35f52a4e788e1cf7469f89385d1bbe049afc46ac
Dev <john.himawan@onextech.com>
2023-06-14 16:56:59 +0800

    feat: remove duplicate style code

--- packages/pages/src/core/LegalPage.tsx
+++ packages/pages/src/core/LegalPage.tsx
@@ -32,29 +32,8 @@ const LegalPage: React.FC<LegalPageProps> = (props) => {
               type: 'html',
               title: html,
               titleProps: {
-                color: 'text.primary', sx: {
-                  '& h1, & h2, & h3, & h4, & h5, & h6': {
-                    mt: 0,
-                    mb: 1,
-                  },
-                  '& h1': {
-                    fontFamily: 'h1.fontFamily',
-                  },
-                  '& h2': {
-                    fontFamily: 'h2.fontFamily',
-                  },
-                  '& h3': {
-                    fontFamily: 'h3.fontFamily',
-                  },
-                  '& h4': {
-                    fontFamily: 'h4.fontFamily',
-                  },
-                  '& h5': {
-                    fontFamily: 'h5.fontFamily',
-                  },
-                  '& h6': {
-                    fontFamily: 'h6.fontFamily',
-                  },
+                color: 'text.primary',
+                sx: {
                   '& p': {
                     mt: 0,
                     mb: 3,
