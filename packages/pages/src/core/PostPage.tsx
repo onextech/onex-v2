3bbb6b49ff77545e73bfd4e27c0ff26d9bb10047
benzlui <benjamin.lui@onextech.com>
2023-07-05 10:38:31 +0800

    feat: add other insights to post page

--- packages/pages/src/core/PostPage.tsx
+++ packages/pages/src/core/PostPage.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Blocks,
   renderPostDetailBlock,
   renderPostHeroBlockItem,
   renderRelatedPostsBlock,
   renderRelatedServicesBlock,
-  useLayout,
+  renderOtherPostsBlock,
 } from '@gravis-os/landing'
 import type { Post, PostCategory, Service } from '@gravis-os/types'
+import isNil from 'lodash/isNil'
+import { getNRandomPosts } from '../utils'
 
 export interface PostPageProps {
   post: Post
   postCategory: PostCategory
   relatedPosts?: Post[]
   relatedServices?: Service[]
+  otherPosts: Post[]
 }
 
+const NUMBER_OF_OTHER_POSTS = 3
+
 const PostPage: React.FC<PostPageProps> = (props) => {
-  const { post, postCategory, relatedPosts, relatedServices } = props
-  const { routeConfig } = useLayout()
+  const { post, relatedPosts, relatedServices, otherPosts } = props
+
+  const [randomPosts, setRandomPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    if (isNil(otherPosts)) return
+    setRandomPosts(getNRandomPosts(otherPosts, NUMBER_OF_OTHER_POSTS))
+  }, [otherPosts])
 
   return (
     <Blocks
@@ -33,6 +44,7 @@ const PostPage: React.FC<PostPageProps> = (props) => {
           sx: { backgroundColor: 'background.paper' },
         }),
         renderRelatedPostsBlock({ items: relatedPosts, py: { xs: 5, md: 10 } }),
+        renderOtherPostsBlock({ items: randomPosts, py: { xs: 5, md: 10 } }),
       ]}
     />
   )
