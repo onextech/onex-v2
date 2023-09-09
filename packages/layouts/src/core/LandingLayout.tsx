f862782eedbb856ccb33e43e2b309ce850285bd8
clodal <joel@onextech.com>
2023-09-09 17:11:43 +0800

    fix(layouts): add hreflang tag to optimize seo

--- packages/layouts/src/core/LandingLayout.tsx
+++ packages/layouts/src/core/LandingLayout.tsx
@@ -620,6 +620,13 @@ const LandingLayout: React.FC<LandingLayoutProps> = (props) => {
         url: `${site.absolute_url}${router.asPath}`,
       },
       canonical: `${site.absolute_url}${router.asPath.split('?')[0]}`,
+      // Add hreflang tags if locales are available
+      ...(Boolean(site.locales?.length) && {
+        languageAlternates: site.locales?.map(({ iso_alpha_2 }) => ({
+          hrefLang: iso_alpha_2,
+          href: `${site.absolute_url}/${iso_alpha_2}${router.asPath}`,
+        })),
+      }),
     },
     headerProps: {
       accordionProps: { titleProps: { variant: 'h5' } },
