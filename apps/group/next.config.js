f69a16c4b555c5fb22c845c975c726ce0cb29e8a
clodal <joel@onextech.com>
2023-09-25 18:11:36 +0800

    feat: update security headers

--- apps/group/next.config.js
+++ apps/group/next.config.js
@@ -33,10 +33,16 @@ module.exports = withBundleAnalyzer({
       {
         source: '/(.*)?', // Matches all pages
         headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          }
+          // No click-jacking
+          { key: 'X-Frame-Options', value: 'DENY' },
+          // No sniffing
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          // No cross-site scripting (XSS)
+          { key: 'Content=Security-Policy', value: "default-src 'self'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com';" },
+          // No permissions
+          { key: 'Permissions-Policy', value: "camera=(); battery=(self); geolocation=(); microphone=()" },
+          // No referrer
+          { key: 'Referrer-Policy', value: "origin-when-cross-origin" },
         ]
       }
     ]
